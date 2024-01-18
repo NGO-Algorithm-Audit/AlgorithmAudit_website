4ad0ffb3666ac0c681ca4e19d27c7832ea72eb68
Erik Torck <eptorck@hotmail.com>
2024-01-18 13:27:12 +0100

    add bias scan templates

--- tina/collections/pages/page.ts
+++ tina/collections/pages/page.ts
@@ -21,7 +21,129 @@ export default {
             type: "rich-text",
             name: "body",
             label: "Content",
-            isBody: true
+            isBody: true,
+            templates: [
+                {
+                    name: 'bias_scan_info',
+                    label: 'Bias Scan info',
+                    match: {
+                        start: '{{<',
+                        end: '>}}',
+                        name: 'bias_scan_info'
+                    },
+                    fields: [
+                        {
+                            name: 'title',
+                            label: 'Title',
+                            type: 'string',
+                            description: '',
+                            required: true,
+                        }
+                    ]
+                },
+                {
+                    name: 'bias_scan_alert_case_study',
+                    label: 'Bias Scan case study',
+                    match: {
+                        start: '{{<',
+                        end: '>}}',
+                        name: 'bias_scan_alert_case_study'
+                    },
+                    fields: [
+                        {
+                            name: 'title',
+                            label: 'Title',
+                            type: 'string',
+                            description: '',
+                            required: true,
+                        }
+                    ]
+                },
+                {
+                    name: 'bias_scan_upload',
+                    label: 'Bias Scan upload',
+                    match: {
+                        start: '{{<',
+                        end: '>}}',
+                        name: 'bias_scan_upload'
+                    },
+                    fields: [
+                        {
+                            name: 'title',
+                            label: 'Title',
+                            type: 'string',
+                            description: '',
+                            required: true,
+                        },
+                        {
+                            type: "image",
+                            name: "css",
+                            label: "Bias Detection Tool css",
+                            required: true,
+                        },
+                        {
+                            type: "image",
+                            name: "js",
+                            label: "Bias Detection Tool js",
+                            required: true,
+                        }
+                    ]
+                },
+                {
+                    name: 'bias_scan_input_data',
+                    label: 'Bias Scan input data',
+                    match: {
+                        start: '{{<',
+                        end: '>}}',
+                        name: 'bias_scan_input_data'
+                    },
+                    fields: [
+                        {
+                            name: 'title',
+                            label: 'Title',
+                            type: 'string',
+                            description: '',
+                            required: true,
+                        }
+                    ]
+                },
+                {
+                    name: 'bias_scan_example_reports',
+                    label: 'Bias Scan example reports',
+                    match: {
+                        start: '{{<',
+                        end: '>}}',
+                        name: 'bias_scan_example_reports'
+                    },
+                    fields: [
+                        {
+                            name: 'title',
+                            label: 'Title',
+                            type: 'string',
+                            description: '',
+                            required: true,
+                        }
+                    ]
+                },
+                {
+                    name: 'bias_scan_faq',
+                    label: 'Bias Scan FAQ',
+                    match: {
+                        start: '{{<',
+                        end: '>}}',
+                        name: 'bias_scan_faq'
+                    },
+                    fields: [
+                        {
+                            name: 'title',
+                            label: 'Title',
+                            type: 'string',
+                            description: '',
+                            required: true,
+                        }
+                    ]
+                }
+            ]
         }
     ],
     format: "md"
